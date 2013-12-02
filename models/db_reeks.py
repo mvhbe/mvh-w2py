67427e1ff129c360f3bdd02b549b27e72ea9c165
Guido Van Hoof <guido.van.hoof3@telenet.be>
2013-12-02 20:07:23 +0100

    reeks toegevoegd

--- models/db_reeks.py
+++ models/db_reeks.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+db.define_table('reeks',
+                Field("wedstrijd", "reference wedstrijd", required=True),
+                Field('reeksnummer', 'integer'),
+                Field('aanvang', 'time', requires=IS_TIME(), default="13:30"),
+                Field('duur', 'time', requires=IS_TIME(), default="02:00"),
+                Field('opmerkingen', 'text'),
+                auth.signature)
