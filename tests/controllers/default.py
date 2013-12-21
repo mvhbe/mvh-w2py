06df93d2c6ce6ebd06dd1c8d09ddb6effd992ed6
Guido Van Hoof <guido.van.hoof3@telenet.be>
2013-12-21 11:30:47 +0100

    default/index : unit testen

--- tests/controllers/default.py
+++ tests/controllers/default.py
@@ -1,39 +1,51 @@
 import datetime
 import unittest
+from dateutil import relativedelta
 from gluon.globals import Request, Session, Storage, Response
 from testhelper import form_postvars
 
 db = setupTestDb() # Use the test database for all tests
 
-vandaag = datetime.date(datetime.datetime.now())
-een_maand_geleden = vandaag - 30
-een_maand_verder = vandaag + 30
+VANDAAG = datetime.datetime.now().date()
+EEN_MAAND_GELEDEN = VANDAAG - relativedelta.relativedelta(months=-1)
+EEN_MAAND_VERDER = VANDAAG + relativedelta.relativedelta(months=+1)
 
-def niewe_wedstrijd(datum):
-    pass
+def nieuweWedstrijd(kalender=1,datum=VANDAAG,
+                    omschrijving="Leden wedstrijd " +
+                                 VANDAAG.strftime("%d/%m/%Y")):
+    db.wedstrijd.insert(kalender=kalender, datum=datum,
+                            omschrijving=omschrijving)
+    db.commit()
+
 
 class DefaultController(unittest.TestCase):
 
     def setUp(self):
         request = Request({})
 
+
+    def tearDown(self):
+        db.wedstrijd.delete()
+        db.commit()
+
+
     def testIndexGeeftGeenWedstrijdenTerugBijLegeDb(self):
         """Geen data aanwezig"""
         response = index()
-
         self.assertEqual(0, len(response["wedstrijden"]))
 
 
     def testIndexGeeftWedstrijdHuidigeMaandTerug(self):
         """Wedstrijddatum huidige maand wordt opgehaald"""
-        pass
+        nieuweWedstrijd()
+        self.assertEqual(1, len(response["wedstrijden"]))
 
 
     def testIndexGeeftWedstrijdVolgendeMaandNietTerug(self):
         """Wedstrijddatum volgende maand wordt niet opgehaald"""
-        pass
+        self.fail("to be implemented")
 
 
     def testIndexGeeftWedstrijdVorigeMaandNietTerug(self):
         """Wedstrijddatum vorige maand wordt niet opgehaald"""
-        pass
+        self.fail("to be implemented")
